5c4d14f113ef14cfef780bb506a8f44193fa7e97
MILIJIC_Esercizio <10934181@itisrossi.vi.it>
2025-12-05 07:11:24 +0100

    Finito

--- GiocoDellaVitaFattoMeglio/CPersonaggio.cs
+++ GiocoDellaVitaFattoMeglio/CPersonaggio.cs
@@ -1,126 +1,88 @@
 ﻿using System;
 using System.Drawing;
 using System.IO;
-using System.Security.Cryptography.X509Certificates;
 using System.Threading;
+using GiocoDellaVitaFattoMeglio;
 
 namespace GiocoDellaVitaFattoMeglio
 {
-
     public abstract class CPersonaggio
     {
-        public new ToolTip tooltip = new();
         public string Nome { get; set; } = string.Empty;
         public int Colonne { get; set; }
         public int Righe { get; set; }
         public int Energia { get; set; }
+        public bool State { get; set; }
         public Image? Immagine { get; set; }
 
-        public event Action<CPersonaggio, CPersonaggio>? OnMangiatoAnimale; 
-        public event Action<CPersonaggio, CPersonaggio>? OnMangiatoCibo;       
+        public event Action<CPersonaggio, CPersonaggio>? OnMangiatoAnimale;
+        public event Action<CPersonaggio, CPersonaggio>? OnMangiatoCibo;
         public event Action<CPersonaggio>? OnMorte;
 
-        public int TurniSenzaCaccia { get; set; } = 0; // per il leone
+        public int TurniSenzaCaccia { get; set; } = 0;
 
-        public abstract void PossoMuovermi();
+        public abstract int EnergiaMinimaPerMuoversi { get; }
 
-     
-        public void MangiaAnimale(CPersonaggio preda)
+        public void PossoMuovermi()
         {
-            OnMangiatoAnimale?.Invoke(this, preda);
+            State = Energia >= EnergiaMinimaPerMuoversi;
         }
 
-        
-        public void MangiaCibo(CPersonaggio cibo)
-        {
-            OnMangiatoCibo?.Invoke(this, cibo);
-        }
-
-        public void Muori()
-        {
-            OnMorte?.Invoke(this);
-        }
+        public void MangiaAnimale(CPersonaggio preda) => OnMangiatoAnimale?.Invoke(this, preda);
+        public void MangiaCibo(CPersonaggio cibo) => OnMangiatoCibo?.Invoke(this, cibo);
+        public void Muori() => OnMorte?.Invoke(this);
     }
-
-
 
     public class CLeone : CPersonaggio
     {
         private static int conteggio = 0;
-
         public CLeone()
         {
             Nome = $"Leone{Interlocked.Increment(ref conteggio)}";
             Immagine = ImmagineHelper.CaricaImmagine("leone.png");
-          
+            Energia = 10;
         }
-
-        public override void PossoMuovermi()
-        {
-            if (Energia < 5)
-            {
-
-            }
-                
-        }
+        public override int EnergiaMinimaPerMuoversi => 5;
     }
-
 
     public class CGazzella : CPersonaggio
     {
         private static int conteggio = 0;
-
         public CGazzella()
         {
             Nome = $"Gazzella{Interlocked.Increment(ref conteggio)}";
             Immagine = ImmagineHelper.CaricaImmagine("gazzella.png");
+            Energia = 6;
         }
-
-        public override void PossoMuovermi()
-        {
-            if (Energia < 4)
-                Console.WriteLine($"{Nome}: Energia troppo bassa ({Energia}) – non mi muovo!");
-        }
+        public override int EnergiaMinimaPerMuoversi => 3;
     }
 
-  
     public class CConiglio : CPersonaggio
     {
         private static int conteggio = 0;
-
         public CConiglio()
         {
             Nome = $"Coniglio{Interlocked.Increment(ref conteggio)}";
             Immagine = ImmagineHelper.CaricaImmagine("coniglio.png");
+            Energia = 4;
         }
-
-        public override void PossoMuovermi()
-        {
-            if (Energia < 2)
-                Console.WriteLine($"{Nome}: Energia troppo bassa ({Energia}) – non mi muovo!");
-        }
+        public override int EnergiaMinimaPerMuoversi => 2;
     }
-
- 
-
-    
 
     public class CCarota : CPersonaggio
     {
-
         public CCarota()
         {
             Energia = 3;
             Immagine = ImmagineHelper.CaricaImmagine("carota.png");
+            State = false;
         }
-            public override void PossoMuovermi()
-        {
-            
-        }
-    
-    }
 
-   
+        public override int EnergiaMinimaPerMuoversi => 3;
+        }
+
+       
+    }
 
     public class CFogliame : CPersonaggio
     {
@@ -128,23 +90,19 @@ namespace GiocoDellaVitaFattoMeglio
         {
             Energia = 5;
             Immagine = ImmagineHelper.CaricaImmagine("fogliame.png");
+            State = false;
         }
 
-            public override void PossoMuovermi()
-        {
-           
+        public override int EnergiaMinimaPerMuoversi => 3;
         }
-    }
+
+       
     
-    
+
     internal static class ImmagineHelper
-    {
-        public static Image? CaricaImmagine(string nomeFile)
-        {
-            DirectoryInfo dirinf = new DirectoryInfo(Directory.GetCurrentDirectory());
+    { public static Image? CaricaImmagine(string nomeFile) 
+        { DirectoryInfo dirinf = new DirectoryInfo(Directory.GetCurrentDirectory()); 
             string path = dirinf.Parent.Parent.FullName + @"\images\" + nomeFile.ToLower();
-          
-
             if (File.Exists(path))
             {
                 try
@@ -153,12 +111,11 @@ namespace GiocoDellaVitaFattoMeglio
                 }
                 catch (Exception ex)
                 {
-                    Console.WriteLine($"Errore caricamento {path}: {ex.Message}");
-                }
-            }
+                    Console.WriteLine($"Errore caricamento {path}: {ex.Message}"); 
 
-            Console.WriteLine($"Immagine NON trovata: {nomeFile}");
-            return null; 
+                }
+            } Console.WriteLine($"Immagine NON trovata: {nomeFile}"); 
+            
+            return null;
         }
     }
-}
--- GiocoDellaVitaFattoMeglio/Form1.cs
+++ GiocoDellaVitaFattoMeglio/Form1.cs
@@ -5,23 +5,188 @@ namespace GiocoDellaVitaFattoMeglio
 {
     public partial class Form1 : Form
     {
-       
         List<CPersonaggio> Personaggi = new List<CPersonaggio>();
         private PictureBox[,] mappa = new PictureBox[10, 10];
         private Random rnd = new Random();
+        private HashSet<CLeone> leoniCheHannoMangiatoQuestoTurno = new();
+        private int turniDaUltimoMangiabile = 0;
+        private const int TURNI_PER_NUOVO_CIBO = 2;
 
 
         public Form1()
         {
-            CreaGiocatori();
             InitializeComponent();
-            CreaMangiabili(6);
             CreaGriglia();
+            inizializzaEventiPersonaggi();
+            InizializzaChat();
         }
+
+        private void HandlerMorte(CPersonaggio morto)
+        {
+            if (IsPositionValid(morto.Righe, morto.Colonne))
+                mappa[morto.Righe, morto.Colonne].Image = null;
+
+            if (Personaggi.Contains(morto))
+                Personaggi.Remove(morto);
+
+            Log($"{morto.Nome} è morto.");
+
+            // Controlla chi ha vinto
+            if (!Personaggi.OfType<CLeone>().Any())
+                Log("Tutti i leoni sono morti! Vincono gli erbivori!");
+            else if (!Personaggi.OfType<CGazzella>().Any() && !Personaggi.OfType<CConiglio>().Any())
+                Log("Tutti gli erbivori sono morti! Vincono i leoni!");
+            VerificaVittoria();
+        }
+
+
+        private void HandlerMangiatoCibo(CPersonaggio erbivoro, CPersonaggio cibo)
+        {
+            if (IsPositionValid(cibo.Righe, cibo.Colonne))
+                mappa[cibo.Righe, cibo.Colonne].Image = null;
+
+            if (Personaggi.Contains(cibo))
+                Personaggi.Remove(cibo);
+
+            erbivoro.Energia += 2;
+
+            // Log dell'azione
+            Log($"{erbivoro.Nome} ha mangiato {cibo.Nome}!");
+
+            turniDaUltimoMangiabile++;
+
+            if (turniDaUltimoMangiabile >= TURNI_PER_NUOVO_CIBO)
+            {
+                CPersonaggio nuovo = CFactory.Crea(Personaggio.Carota);
+                CPersonaggio nuovo2 = CFactory.Crea(Personaggio.Fogliame);
+
+                Personaggi.Add(nuovo);
+                Personaggi.Add(nuovo2);
+
+                nuovo.OnMangiatoAnimale += HandlerMangiatoAnimale;
+                nuovo.OnMangiatoCibo += HandlerMangiatoCibo;
+                nuovo.OnMorte += HandlerMorte;
+
+                nuovo2.OnMangiatoAnimale += HandlerMangiatoAnimale;
+                nuovo2.OnMangiatoCibo += HandlerMangiatoCibo;
+                nuovo2.OnMorte += HandlerMorte;
+
+                PosizionaElementoCasuale(nuovo);
+                PosizionaElementoCasuale(nuovo2);
+
+                Log("Sono comparsi nuovi cibi: Carota e Fogliame");
+
+                turniDaUltimoMangiabile = 0;
+            }
+
+            AggiornaGriglia();
+        }
+
+
+
+
+        private void PosizionaElementoCasuale(CPersonaggio p)
+        {
+            List<(int r, int c)> libere = new();
+
+            for (int r = 0; r < mappa.GetLength(0); r++)
+            {
+                for (int c = 0; c < mappa.GetLength(1); c++)
+                {
+                    if (!Personaggi.Any(x => x.Righe == r && x.Colonne == c))
+                    {
+                        libere.Add((r, c));
+                    }
+                }
+            }
+
+            if (libere.Count == 0) return;
+
+            var pos = libere[rnd.Next(libere.Count)];
+            p.Righe = pos.r;
+            p.Colonne = pos.c;
+
+            if (p.Immagine != null)
+                mappa[pos.r, pos.c].Image = p.Immagine;
+        }
+
+
+        private void VerificaVittoria()
+        {
+            int leoniVivi = Personaggi.OfType<CLeone>().Count();
+            int erbivoriVivi = Personaggi.OfType<CGazzella>().Count() + Personaggi.OfType<CConiglio>().Count();
+
+            Log($"Animali in vita: Leoni={leoniVivi}, Erbivori={erbivoriVivi}");
+
+            if (leoniVivi == 0)
+            {
+                Log("Tutti i leoni sono morti. Gli erbivori vincono!");
+                MessageBox.Show("I leoni sono morti, vincono le prede");
+
+                DisabilitaMovimento();
+            }
+            else if (erbivoriVivi == 0)
+            {
+                Log("Tutti gli erbivori sono morti. I leoni vincono!");
+                MessageBox.Show("Le prede sono morte, vincono i leoni");
+                DisabilitaMovimento();
+            }
+        }
+
+
+        // Disabilita il pulsante di movimento quando il gioco finisce
+        private void DisabilitaMovimento()
+        {
+            MuoviGioco.Enabled = false;
+        }
+
+
+        private void HandlerMangiatoAnimale(CPersonaggio predatore, CPersonaggio preda)
+        {
+            if (IsPositionValid(preda.Righe, preda.Colonne))
+                mappa[preda.Righe, preda.Colonne].Image = null;
+
+            if (Personaggi.Contains(preda))
+                Personaggi.Remove(preda);
+
+            // Log dell'azione
+            Log($"{predatore.Nome} ha mangiato {preda.Nome}!");
+
+            if (predatore is CLeone leone)
+            {
+                predatore.Energia += 3;
+                leone.TurniSenzaCaccia = 0;
+                leoniCheHannoMangiatoQuestoTurno.Add(leone);
+            }
+            else if (predatore is CGazzella || predatore is CConiglio)
+            {
+                predatore.Energia += 1;
+            }
+        }
+
+
+
+        private void inizializzaEventiPersonaggi()
+        {
+            foreach (var p in Personaggi)
+            {
+                p.OnMangiatoAnimale -= HandlerMangiatoAnimale;
+                p.OnMangiatoCibo -= HandlerMangiatoCibo;
+                p.OnMorte -= HandlerMorte;
+            }
+
+            foreach (var p in Personaggi)
+            {
+                p.OnMangiatoAnimale += HandlerMangiatoAnimale;
+                p.OnMangiatoCibo += HandlerMangiatoCibo;
+                p.OnMorte += HandlerMorte;
+            }
+        }
+
         private void CreaGriglia()
         {
-            int righe = 10;
-            int colonne = 10;
+            int righe = mappa.GetLength(0);
+            int colonne = mappa.GetLength(1);
             int cellSize = 60;
             int padding = 2;
 
@@ -32,173 +197,252 @@ namespace GiocoDellaVitaFattoMeglio
                     PictureBox pb = new PictureBox();
                     pb.Width = cellSize;
                     pb.Height = cellSize;
-
                     pb.BorderStyle = BorderStyle.FixedSingle;
                     pb.SizeMode = PictureBoxSizeMode.StretchImage;
-
-                    pb.Location = new Point(
-                        c * (cellSize + padding),
-                        r * (cellSize + padding)
-                    );
-
+                    pb.Location = new Point(c * (cellSize + padding), r * (cellSize + padding));
                     mappa[r, c] = pb;
                     MappaDiGioco.Controls.Add(pb);
                 }
             }
         }
 
+        private bool IsPositionValid(int r, int c) => r >= 0 && r < mappa.GetLength(0) && c >= 0 && c < mappa.GetLength(1);
 
         public void PosizionaLeCose()
         {
-            int size = 10;
-
-            // Unifica tutti gli elementi da posizionare
-            var tutti = new List<dynamic>();
-            tutti.AddRange(Personaggi);
-           
-
-            // Genera tutte le posizioni disponibili (100 celle)
+            var tutti = new List<CPersonaggio>(Personaggi);
             List<(int r, int c)> posizioni = new List<(int, int)>();
-            for (int r = 0; r < size; r++)
-            for (int c = 0; c < size; c++)
-                posizioni.Add((r, c));
 
-            // Mescola una volta sola usando il Random di classe
+            for (int r = 0; r < mappa.GetLength(0); r++)
+                for (int c = 0; c < mappa.GetLength(1); c++)
+                    posizioni.Add((r, c));
+
             posizioni = posizioni.OrderBy(x => rnd.Next()).ToList();
 
-            // Svuota la griglia
-            for (int r = 0; r < size; r++)
-            for (int c = 0; c < size; c++)
-                mappa[r, c].Image = null;
-
-            // Assegna una posizione casuale diversa a ciascun animale/mangiabile
             for (int i = 0; i < tutti.Count && i < posizioni.Count; i++)
             {
                 var elem = tutti[i];
                 var (r, c) = posizioni[i];
-
                 elem.Righe = r;
                 elem.Colonne = c;
-
-                if (elem.Immagine != null)
-                    mappa[r, c].Image = elem.Immagine;
             }
-            
+
+            AggiornaGriglia();
         }
 
-        private void VerificaNuovePosizioni(object sender, EventArgs e)
+        private void VerificaNuovePosizioni()
         {
+            var azioni = new List<(bool animaleMangiaAnimale, CPersonaggio attore, CPersonaggio bersaglio)>();
             for (int i = 0; i < Personaggi.Count; i++)
             {
                 for (int j = i + 1; j < Personaggi.Count; j++)
                 {
-                    // Stessa posizione
-                    if ((Personaggi[i].Colonne == Personaggi[j].Colonne &&
-                        Personaggi[i].Righe == Personaggi[j].Righe))
+                    if (Personaggi[i].Righe == Personaggi[j].Righe && Personaggi[i].Colonne == Personaggi[j].Colonne)
                     {
-                        // Leone + (Gazzella o Coniglio)
-                        bool coppiaLeonePreda =
-                            (Personaggi[i] is CLeone && (Personaggi[j] is CGazzella || Personaggi[j] is CConiglio)) ||
-                            (Personaggi[j] is CLeone && (Personaggi[i] is CGazzella || Personaggi[i] is CConiglio));
-
-                        bool coppiaAnimaleErbivoroCibo =
-                            ((Personaggi[i] is CGazzella || Personaggi[i] is CConiglio)&& (Personaggi[j] is CFogliame || Personaggi[j] is CCarota)) ||
-                            ((Personaggi[j] is CGazzella || Personaggi[j] is CConiglio)&& (Personaggi[i] is CFogliame || Personaggi[i] is CCarota));
-                        
-                        if (coppiaLeonePreda)
-                        {
-                            // Azione leone–preda
-                        }
-                        else if (coppiaAnimaleErbivoroCibo)
-                        {
-                            // Azione Erbivoro-Cibo
-                        }
+                        CPersonaggio a = Personaggi[i];
+                        CPersonaggio b = Personaggi[j];
+                        if (a is CLeone && (b is CGazzella || b is CConiglio)) azioni.Add((true, a, b));
+                        else if (b is CLeone && (a is CGazzella || a is CConiglio)) azioni.Add((true, b, a));
+                        else if ((a is CGazzella || a is CConiglio) && (b is CCarota || b is CFogliame)) azioni.Add((false, a, b));
+                        else if ((b is CGazzella || b is CConiglio) && (a is CCarota || a is CFogliame)) azioni.Add((false, b, a));
                     }
                 }
             }
-        }
 
-
-        private void MuoviCasuale()
-        {
-           foreach(var animale in Personaggi)
+            foreach (var (animaleMangiaAnimale, attore, bersaglio) in azioni)
             {
-                animale.PossoMuovermi();
+                if (animaleMangiaAnimale) attore.MangiaAnimale(bersaglio);
+                else attore.MangiaCibo(bersaglio);
             }
-            VerificaNuovePosizioni();
+
+            AggiornaGriglia();
         }
 
-
-
-        public void GeneraMangiabili()
+        private void GestisciFameLeoni()
         {
+            const int MAX_TURNI_SENZA_CACCIA = 10;
+            var leoni = Personaggi.OfType<CLeone>().ToList();
+            foreach (var leone in leoni)
+            {
+                if (leoniCheHannoMangiatoQuestoTurno.Contains(leone))
+                {
+                    leoniCheHannoMangiatoQuestoTurno.Remove(leone);
+                    continue;
+                }
+                leone.TurniSenzaCaccia++;
+                if (leone.TurniSenzaCaccia > MAX_TURNI_SENZA_CACCIA) leone.Muori();
+            }
         }
+
+        private void MovimentoCasuale()
+        {
+            List<CPersonaggio> daRimuovere = new List<CPersonaggio>();
+
+            foreach (var p in Personaggi.ToList())
+            {
+                p.Energia += 1; // energia passiva per turno
+                p.PossoMuovermi();
+                if (!p.State) continue;
+
+                int nuovaR = rnd.Next(mappa.GetLength(0));
+                int nuovaC = rnd.Next(mappa.GetLength(1));
+
+                p.Righe = nuovaR;
+                p.Colonne = nuovaC;
+                p.Energia -= 1;
+                if (p.Energia <= 0) daRimuovere.Add(p);
+
+            }
+
+            VerificaNuovePosizioni(); // gestisce mangiate
+            GestisciFameLeoni();
+
+            foreach (var morto in daRimuovere)
+                morto.Muori();
+
+            AggiornaGriglia();
+        }
+
+
+        private void AggiornaGriglia()
+        {
+            for (int r = 0; r < mappa.GetLength(0); r++)
+                for (int c = 0; c < mappa.GetLength(1); c++)
+                    mappa[r, c].Image = null;
+
+            foreach (var p in Personaggi)
+                if (IsPositionValid(p.Righe, p.Colonne) && p.Immagine != null)
+                    mappa[p.Righe, p.Colonne].Image = p.Immagine;
+        }
+
         public int CreaMangiabili(int quante)
         {
-            if (quante <= 0)
-            {
-                MessageBox.Show("Quantità di carote non valida.");
-                return 0;
-            }
+            if (quante <= 0) return 0;
 
             for (int i = 0; i < quante; i++)
             {
-                // creo una nuova carota tramite factory
                 CPersonaggio nuova = CFactory.Crea(Personaggio.Carota);
                 CPersonaggio nuova1 = CFactory.Crea(Personaggio.Fogliame);
 
-                // aggiungo alla lista
                 Personaggi.Add(nuova);
                 Personaggi.Add(nuova1);
+
+                nuova.OnMangiatoAnimale += HandlerMangiatoAnimale;
+                nuova.OnMangiatoCibo += HandlerMangiatoCibo;
+                nuova.OnMorte += HandlerMorte;
+
+                nuova1.OnMangiatoAnimale += HandlerMangiatoAnimale;
+                nuova1.OnMangiatoCibo += HandlerMangiatoCibo;
+                nuova1.OnMorte += HandlerMorte;
             }
 
             return quante;
         }
+        private const int MaxRighe = 8; // numero di righe nella chat
+
+        private void InizializzaChat()
+        {
+            ChatDiAggiornamento.RowCount = MaxRighe;
+            ChatDiAggiornamento.ColumnCount = 1;
+            ChatDiAggiornamento.Controls.Clear();
+            ChatDiAggiornamento.RowStyles.Clear();
+
+            for (int r = 0; r < MaxRighe; r++)
+            {
+                ChatDiAggiornamento.RowStyles.Add(new RowStyle(SizeType.Percent, 100f / MaxRighe));
+
+                Label lbl = new Label
+                {
+                    Text = string.Empty,
+                    AutoSize = false,
+                    TextAlign = ContentAlignment.MiddleLeft,
+                    Dock = DockStyle.Fill,
+                    ForeColor = Color.Black,
+                    BackColor = Color.Transparent // sfondo trasparente
+                };
+
+                ChatDiAggiornamento.Controls.Add(lbl, 0, r);
+            }
+        }
+
+
+        private void Log(string messaggio)
+        {
+            // Sposta il contenuto verso l’alto
+            for (int r = 0; r < MaxRighe - 1; r++)
+            {
+                if (ChatDiAggiornamento.GetControlFromPosition(0, r) is Label lbl &&
+                    ChatDiAggiornamento.GetControlFromPosition(0, r + 1) is Label lblSucc)
+                {
+                    lbl.Text = lblSucc.Text;
+                }
+            }
+
+            // Inserisci il nuovo messaggio nell’ultima label
+            if (ChatDiAggiornamento.GetControlFromPosition(0, MaxRighe - 1) is Label ultima)
+            {
+                ultima.Text = messaggio;
+            }
+        }
+
+
+
+
+
+
 
 
         public void CreaGiocatori()
         {
             Personaggi.Add(CFactory.Crea(Personaggio.Leone));
             Personaggi.Add(CFactory.Crea(Personaggio.Leone));
+            Personaggi.Add(CFactory.Crea(Personaggio.Leone));
+            Personaggi.Add(CFactory.Crea(Personaggio.Leone));
             Personaggi.Add(CFactory.Crea(Personaggio.Gazzella));
             Personaggi.Add(CFactory.Crea(Personaggio.Gazzella));
             Personaggi.Add(CFactory.Crea(Personaggio.Coniglio));
             Personaggi.Add(CFactory.Crea(Personaggio.Coniglio));
             Personaggi.Add(CFactory.Crea(Personaggio.Coniglio));
-        }
-
-
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void MappaDiGioco_Paint(object sender, PaintEventArgs e)
-        {
-
-        }
-
-        private void ChatDiAggiornamento_Paint(object sender, PaintEventArgs e)
-        {
-
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
         }
 
         private void IniziaGioco_Click(object sender, EventArgs e)
         {
-            
+            Log("Partita iniziata!");
+            // Pulisci personaggi e mappa
+            Personaggi.Clear();
+            for (int r = 0; r < mappa.GetLength(0); r++)
+            for (int c = 0; c < mappa.GetLength(1); c++)
+                mappa[r, c].Image = null;
+
+            // Pulisci la chat
+            foreach (Control ctrl in ChatDiAggiornamento.Controls)
+            {
+                if (ctrl is Label lbl)
+                    lbl.Text = string.Empty;
+            }
+
+            // Crea nuovi elementi di gioco
+            CreaGiocatori();
+            CreaMangiabili(6);
+            inizializzaEventiPersonaggi();
             PosizionaLeCose();
-            
+            MuoviGioco.Enabled = true;
+
         }
 
         private void MuoviGioco_Click(object sender, EventArgs e)
         {
-            MuoviCasuale();
+            MovimentoCasuale();
+            if (Personaggi.Count == 0)
+            {
+                MessageBox.Show("Inizia prima una partita");
+                
+            }
+            
         }
+        private void label1_Click(object sender, EventArgs e) { }
+        private void MappaDiGioco_Paint(object sender, PaintEventArgs e) { }
+        private void ChatDiAggiornamento_Paint(object sender, PaintEventArgs e) { }
+        private void Form1_Load(object sender, EventArgs e) { }
     }
 }
